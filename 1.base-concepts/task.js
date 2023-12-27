9d220b5bea3c27d55bc137f7f4a3e6275dd55d09
Alexandr Yaskevich <yask.alexandr@gmail.com>
2023-12-27 19:24:30 +0300

    work of mistakes

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,9 +3,7 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d = b**2 - 4 * a * c;
 
-  if (d < 0) {
-  arr = [];
-  } else if (d === 0) {
+  if (d === 0) {
   arr = [-b / (2 * a)];
   } else if (d > 0) {
   arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
@@ -14,9 +12,9 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let percentZeroOne = percent / 100;
+  let percentZeroOne = (percent / 100) / 12;
   let creditBody = amount - contribution; 
-  let monthsPay = creditBody * (percentZeroOne / 12 + (percentZeroOne / 12 / (((1 + percentZeroOne /12)**countMonths)-1)))
-  let allPay = creditBody + monthsPay;
+  let monthsPay = creditBody * (percentZeroOne + (percentZeroOne / (((1 + percentZeroOne)**countMonths) - 1))) ;
+  let allPay = monthsPay * countMonths;
   return Number(allPay.toFixed(2));
 }
