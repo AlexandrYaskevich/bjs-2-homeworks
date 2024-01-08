dbdac508a83291eb05262a51112c7192028934b3
Alexandr Yaskevich <yask.alexandr@gmail.com>
2024-01-08 17:40:45 +0300

    function first task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,11 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let max = Math.max.apply(null, arr);
+  let min = Math.min.apply(null, arr);
+  let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return {min: Number(min), max: Number(max), avg: Number((sum / arr.length).toFixed(2))};
 }
 
 function summElementsWorker(...arr) {
