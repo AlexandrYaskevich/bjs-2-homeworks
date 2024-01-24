ff052381fa8a53884b020ea482010c92fd4e839f
Alexandr Yaskevich <yask.alexandr@gmail.com>
2024-01-24 22:24:59 +0300

    homework exception-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,44 @@
-﻿
+﻿function parseCount(item) {
+    if(Number.isNaN(Number.parseFloat(item))) {
+        throw new Error ("Невалидное значение");
+    }
+        return Number.parseFloat(item);
+}
+
+function validateCount(item) {
+    try {
+        return parseCount(item);
+        } catch(error) {
+            return error;
+        }
+}
+
+class Triangle {
+    constructor(a, b, c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if(this.a + this.b < this.c || this.b + this.c < this.a || this.c + this.a < this.b) {
+        throw new Error("Треугольник с такими сторонами не существует");
+        }  
+    }
+
+    get perimeter() {
+       return this.a + this.b + this.c;
+    }
+    get area() {
+    let p = (this.a + this.b + this.c)*0.5;
+    return Number((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+        } catch(e) {
+          let triangleForAnswer = new Triangle (a, b, c); 
+          triangleForAnswer.area = "Треугольник с такими сторонами не существует";
+          triangleForAnswer.perimeter = "Треугольник с такими сторонами не существует";
+          return triangleForAnswer;
+    }
+}
